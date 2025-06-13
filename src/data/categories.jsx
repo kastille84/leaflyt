7cbcd00dce64208268e0e43ad2f67c81a89b6fa0
Edwin <kastille84@gmail.com>
2025-06-13 17:07:49 -0400

    CREATED list of categories and subcategories

---
+++ src/data/categories.jsx
@@ -0,0 +1,47 @@
+export default [
+  {
+    category: "Services",
+    subcategories: [
+      "Professional Services",
+      "Personal Development/Instruction",
+      "Automotive Services",
+    ],
+  },
+  {
+    category: "Events & Activities",
+    subcategories: [
+      "Community Events",
+      "Religious/Spiritual Events",
+      "Arts & Entertainment",
+      "Educational Workshops/Seminars",
+    ],
+  },
+  {
+    category: "Goods for Sale/Wanted",
+    subcategories: ["For Sale", "Wanted"],
+  },
+  {
+    category: "Community & Social",
+    subcategories: [
+      "Groups & Clubs",
+      "Volunteer Opportunities",
+      "Lost & Found",
+    ],
+  },
+  {
+    category: "Real Estate & Housing",
+    subcategories: [
+      "Rooms for Rent/Roommates Wanted",
+      "Apartments/Houses for Rent",
+      "Houses for Sale",
+    ],
+  },
+  {
+    category: "Employment & Opportunities",
+    subcategories: ["Job Openings", "Volunteers Needed"],
+  },
+  {
+    category: "Miscellaneous",
+    subcategories: ["Announcements", "Advertisements for online services"],
+  },
+];
--- src/ui/Form/FieldInputError.tsx
+++ src/ui/Form/FieldInputError.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 const StyledError = styled.small`
   color: var(--color-orange-600);
   letter-spacing: 0.4px;
+  font-weight: 700;
 `;
 export default function FieldInputError({
   message,
