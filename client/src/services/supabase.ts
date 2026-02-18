33e53c57bfee325121fd4dcfd6c59640165e599b
Edwin <kastille84@gmail.com>
2026-02-18 07:46:52 -0500

    CHKECKING ENV VARIABLES

--- client/src/services/supabase.ts
+++ client/src/services/supabase.ts
@@ -2,5 +2,7 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 import { keysBasedOnEnv } from "../utils/GeneralUtils";
 
 export const supabaseUrl = keysBasedOnEnv().supabase.url;
+console.log("supabaseUrl - client", supabaseUrl);
+console.log("keysBasedOnEnv() - client", keysBasedOnEnv());
 const supabaseKey = keysBasedOnEnv().supabase.apiKey;
 export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
