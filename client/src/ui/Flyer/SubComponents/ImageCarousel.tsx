9e0fc87e47ac8403344ef2d61c51542ef89a1a8d
Edwin <kastille84@gmail.com>
2026-06-11 15:59:42 -0400

    UPDATED Image handling for flyers

--- client/src/ui/Flyer/SubComponents/ImageCarousel.tsx
+++ client/src/ui/Flyer/SubComponents/ImageCarousel.tsx
@@ -1,46 +1,12 @@
 import styled from "styled-components";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import { useGlobalContext } from "../../../context/GlobalContext";
 import { UploadApiResponse } from "cloudinary";
 import ImageGallery from "react-image-gallery";
 import "react-image-gallery/styles/image-gallery.css";
 import type { GalleryItem, ImageGalleryRef } from "react-image-gallery";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
-const StyledFigure = styled.figure<{
-  height?: number | string;
-  backgroundColor?: string;
-}>`
-  width: 100%;
-  height: ${(props) =>
-    typeof props.height === "number" ? `${props.height}px` : `${props.height}`};
-  background-color: ${(props) =>
-    props.backgroundColor ? props.backgroundColor : "#fff"};
-  /* opacity: 0.85; */
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  @media (max-height: 34em) {
-    width: 50%;
-    height: auto;
-    margin: auto;
-  }
-`;
-const StyledCarouselContainer = styled.div<{ bgColor: string }>`
-  & .react-multi-carousel-item {
-    background-color: ${(props) => props.bgColor};
-  }
-  & [type="button"] {
-    z-index: 99;
-  }
-  & .react-multi-carousel-dot-list {
-    bottom: 15px;
-  }
-`;
-
-const CarouselContainer = styled.div`
+const CarouselContainer = styled.div<{ hide?: boolean }>`
   /* Force uniform container dimensions */
   & .image-gallery-slide {
     /* height: 250px; Set your desired frame height */
@@ -75,6 +41,10 @@ const CarouselContainer = styled.div`
     padding: 0px 6px;
     top: 40%;
   }
+
+  & .image-gallery-fullscreen-button.hide {
+    visibility: hidden;
+  }
 `;
 export default function ImageCarousel({
   images,
@@ -106,41 +76,15 @@ export default function ImageCarousel({
   };
 
   const {
-    setIsOpenBottomSlideIn,
-    bottomSlideInType,
-    setBottomSlideInType,
-    setContextImages,
     contextImages,
   } = useGlobalContext();
 
-  const handleImageClick = (fileUrlArr: UploadApiResponse[]) => {
-    if (fromFlyerBlock && bottomSlideInType !== "hasTemplates") {
-      setIsOpenBottomSlideIn(true);
-      setBottomSlideInType("carousel");
-      setContextImages(images!);
-    }
-  };
   // either use provided images (from flyer block) or context images (from bottom slide-in)
   const filesToUse = fromFlyerBlock ? images : contextImages;
-  // const heightToUse = fromFlyerBlock ? 250 : 800;
-
-  const determineImageWidth = (file: UploadApiResponse) => {
-    // if (file.height > heightToUse) {
-    //   if (fromFlyerBlock) {
-    //     return "300px";
-    //   } else {
-    //     return "450px";
-    //   }
-    // } else {
-    //   return "auto";
-    // }
-
-    return "100%";
-  };
 
   const galleryRef = useRef<ImageGalleryRef>(null);
 
-  const defineGalleryItems = images?.map((file) => ({
+  const defineGalleryItems = filesToUse?.map((file) => ({
     original: file.secure_url,
     thumbnail: file.secure_url,
     isVideo: file.resource_type === "video",
@@ -191,70 +135,41 @@ export default function ImageCarousel({
     );
   };
 
+  function hideFullScreenButtonOnVideoSlide(nextIndex: number) {
+      const fullScreenIcon = document.querySelector(
+        ".image-gallery-fullscreen-button",
+      );
+      // add visibility hidden to full screen button if it's a video slide, otherwise show it
+      fullScreenIcon?.classList.toggle(
+        "hide",
+        defineGalleryItems[nextIndex].isVideo,
+      );
+  }
+
+  useEffect(() => {
+    // on initial load, hide full screen button if first slide is video
+    hideFullScreenButtonOnVideoSlide(0);
+  }, []);
+
   return (
     <CarouselContainer>
       <ImageGallery
         ref={galleryRef}
         items={defineGalleryItems ? defineGalleryItems : ([] as GalleryItem[])}
-        onSlide={(index) => console.log("Slid to", index)}
         showPlayButton={false}
         showThumbnails={false}
         renderItem={renderGalleryItem}
+        useBrowserFullscreen={true}
+        onBeforeSlide={(nextIndex) => {
+          hideFullScreenButtonOnVideoSlide(nextIndex);
+        }}
+        onImageLoad={(event) => {
+          const imgElement = event.target as HTMLImageElement;
+          console.log("imgElement:", imgElement);
+          // imgElement.style.objectFit = "contain";
+          // imgElement.style.objectPosition = "center";
+        }}
       />
     </CarouselContainer>
-  );
-  return (
-    <StyledCarouselContainer
-      bgColor={bgColor ? bgColor : "var(--color-grey-75)"}
-    >
-      <Carousel
-        ssr={false}
-        responsive={responsive}
-        // showDots
-        itemClass="carousel-item"
-        afterChange={(index) => {}}
-      >
-        {filesToUse!.map((file, index) => (
-          // <div
-          //   style={{ height: "450px", objectFit: "cover" }}
-          //   onClick={() => handleImageClick(filesToUse!)}
-          // >
-          //   <img
-          //     src={file.secure_url}
-          //     width={"auto"}
-          //     height={"auto"}
-          //     style={{ objectFit: "contain" }}
-          //   />
-          // </div>
-          <StyledFigure
-            key={index}
-            onClick={() => handleImageClick(filesToUse!)}
-            height={fromFlyerBlock ? 250 : 800}
-            // height={file.height > heightToUse ? "auto" : heightToUse}
-            backgroundColor={file.resource_type === "video" ? bgColor : "#fff"}
-          >
-            {file.resource_type === "video" && (
-              <video
-                controls
-                src={file.secure_url}
-                width={fromFlyerBlock ? "100%" : "60%"}
-                height={fromFlyerBlock ? "250px" : "80%"}
-                onClick={(e) => e.stopPropagation()}
-                onBlur={(e) => e.target.pause()}
-              />
-            )}
-            {file.resource_type === "image" && (
-              <img
-                src={file.secure_url}
-                // width={file.height > heightToUse ? "300px" : "auto"}
-                width={determineImageWidth(file)}
-                height={"auto"}
-                //  style={{ objectFit: "contain" }}
-              />
-            )}
-          </StyledFigure>
-        ))}
-      </Carousel>
-    </StyledCarouselContainer>
   );
 }
